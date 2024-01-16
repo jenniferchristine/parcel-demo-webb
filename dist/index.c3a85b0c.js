b830a36ae531fe3770ea4a889fcc4a5b794ae68c
jenniferchristine <jeja2306@student.miun.se>
2024-01-16 12:05:53 +0100

    Uppdaterade parcel-cache vid felmeddelande

---
+++ dist/index.c3a85b0c.js
@@ -0,0 +1,2 @@
+let e=document.getElementById("open-menu"),n=document.getElementById("close-menu");function t(){let e=document.getElementById("main-nav");"none"===window.getComputedStyle(e).display?e.style.display="block":e.style.display="none"}e.addEventListener("click",t),n.addEventListener("click",t);
+//# sourceMappingURL=index.c3a85b0c.js.map
